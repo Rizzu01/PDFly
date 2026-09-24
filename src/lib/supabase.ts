901e2e4e127f8c9dfab05ded0fcb07a5de6e2d39
Rizzu01 <122160674+Rizzu01@users.noreply.github.com>
2026-09-24 12:57:53 +0530

    Connect PDFly to new Supabase project

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,10 +6,10 @@ import { createClient } from '@supabase/supabase-js';
 // requiring Vercel Environment Variables.
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ??
-  'https://xaaerrvvcfrwtggzwmjh.supabase.co';
+  'https://bcnbpbvezwkdpwrmaraq.supabase.co';
 
 const supabaseAnonKey =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
-  'sb_publishable_nCXiLeksPC9xxVFls-54sQ_5b3TD7t3';
+  'sb_publishable_FQ3x8D6SlvNGYQ-EZSXbMQ_5VOUjFUK';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
